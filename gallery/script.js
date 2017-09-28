8d7cf451cce674dc4cd6f9ba12719e967e0f0034
AtemGrachev <32243933+AtemGrachev@users.noreply.github.com>
2017-09-28 12:17:36 +0300

    Добавлен Infinite scroll по 4 картинки. Jquery используется только для него.

--- gallery/script.js
+++ gallery/script.js
@@ -1,5 +1,6 @@
-window.onload = function() {
+window.onload = function () {
     'use strict';
+
     function CreateRequest() {
         var Request = false;
         if (window.XMLHttpRequest) {
@@ -8,9 +9,9 @@ window.onload = function() {
         } else if (window.ActiveXObject) {
             //Internet explorer
             try {
-                 Request = new ActiveXObject("Microsoft.XMLHTTP");
+                Request = new ActiveXObject("Microsoft.XMLHTTP");
             } catch (CatchException) {
-                 Request = new ActiveXObject("Msxml2.XMLHTTP");
+                Request = new ActiveXObject("Msxml2.XMLHTTP");
             }
         }
         if (!Request) {
@@ -29,7 +30,7 @@ window.onload = function() {
             return;
         }
         // Назначение пользовательского обработчика
-        Request.onreadystatechange = function() {
+        Request.onreadystatechange = function () {
             if (Request.readyState == XMLHttpRequest.DONE) {
                 if (Request.status == 200) {
                     r_handler(Request);
@@ -43,7 +44,7 @@ window.onload = function() {
         Request.open(r_method, r_path, true);
         // Данные запроса
         if (r_method.toLowerCase() == "post") {
-            Request.setRequestHeader("Content-Type","application/x-www-form-urlencoded; charset=utf-8");
+            Request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=utf-8");
             Request.send(r_args);
         } else {
             Request.send(null);
@@ -51,6 +52,7 @@ window.onload = function() {
     }
     // Универсальный класс создания тега
     function Container(myTag, myChildren, myClass, myAttributes, myOptions) {
+        this.tag = myTag;
         this.elem = document.createElement((myTag) ? myTag : 'div');
         if (myChildren instanceof Array) {
             this.cChildren = myChildren;
@@ -78,13 +80,14 @@ window.onload = function() {
     Container.prototype.render = function () {
         if (this.elem) {
             var that = this;
+            this.elem = document.createElement((this.tag) ? this.tag : 'div');
             this.cChildren.forEach(function (child) {
                 if (child instanceof Container) {
                     that.elem.appendChild(child.render());
                 } else if (typeof child == 'object') {
                     that.elem.appendChild(child);
                 } else if (typeof child == 'string') {
-                    that.elem.appendChild(document.createTextNode(child));
+                    that.elem.innerHTML += child;
                 } else {
                     throw new Error("Неправильный тип потомка " + child);
                 }
@@ -110,33 +113,67 @@ window.onload = function() {
             this.elem = null;
         }
     };
-    
+
     function closePopUp(popUp) {
         return function () {
             event.preventDefault();
             popUp.remove();
         }
     }
+
     function writePopUp(sources) {
         return function () {
             var popUp = new Container('div', [], 'popup');
-            var popUpImg = new Container('img', [], 'popup__img',
-                {"src": "img/" + sources.bigImg, "alt": "Обои"});
-            var popUpClose = new Container('button', ["Закрыть"], 'popup__close',
-                {}, {"onclick": closePopUp(popUp)});
+            var popUpImg = new Container('img', [], 'popup__img', {
+                "src": "img/" + sources.bigImg,
+                "alt": "Обои"
+            });
+            var popUpClose = new Container('button', ["Закрыть"], 'popup__close', {}, {
+                "onclick": closePopUp(popUp)
+            });
             popUp.cChildren = [popUpImg, popUpClose];
-            popUp.cOptions = {"onclick": closePopUp(popUp)};
+            popUp.cOptions = {
+                "onclick": closePopUp(popUp)
+            };
             document.getElementsByTagName('body')[0].appendChild(popUp.render());
         }
     }
+    
+    var startScroll = 0;
+    var makeScroll = true;
+    var spinScroll = new Container('i', ["&#xe839;"], 'demo-icon icon-spin6 animate-spin');
+    
     //Обработка AJAX-запроса
     function writeImg(result) {
-        JSON.parse(result.responseText).forEach(function (sources) {
-            var writeImg = new Container('img', [], 'gallery__img',
-                {"src": "img/" + sources.smallImg, "alt": "Миниатюра"},
-                {"onclick": writePopUp(sources)});
-            document.getElementsByClassName('gallery')[0].appendChild(writeImg.render());
-        });
+        if (result.responseText.indexOf('[]') != -1) {
+            scroller = null;
+            document.getElementsByClassName('spinner')[0].removeChild(spinScroll.elem);
+        } else {
+            JSON.parse(result.responseText).forEach(function (sources) {
+                var writeImg = new Container('img', [], 'gallery__img', {
+                    "src": "img/" + sources.smallImg,
+                    "alt": "Миниатюра"
+                }, {
+                    "onclick": writePopUp(sources)
+                });
+                document.getElementsByClassName('gallery')[0].appendChild(writeImg.render());
+            });
+            if (startScroll != 0) {
+                makeScroll = true;
+                document.getElementsByClassName('spinner')[0].removeChild(spinScroll.elem);
+            }
+        }
     }
-    SendRequest('GET', 'files.json', 'time=' + new Date().getTime(), writeImg);
+    var scroller = function () {
+        if (($(window).scrollTop() >= $(document).height() - $(window).height() - 10) && (makeScroll)) {
+            makeScroll = false;
+            document.getElementsByClassName('spinner')[0].appendChild(spinScroll.render());
+            startScroll += 4
+            setTimeout(function () {
+                SendRequest('GET', 'ajax.php', 'time=' + new Date().getTime() + '&start=' + startScroll, writeImg);
+            }, 1000);
+        }
+    };
+    $(window).scroll(scroller);
+    SendRequest('GET', 'ajax.php', 'time=' + new Date().getTime() + '&start=0', writeImg);
 };
